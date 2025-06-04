816ee5bb19c94eb01e08ced393b83c2b814f631a
JoMath363 <JoMath363@users.noreply.github.com>
2025-06-04 19:33:23 -0300

    removed Projects API

--- TasksAPI/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import projectRoute from "./projectRoute.js";
-import taskRoute from "./taskRoute.js";
-
-const router = (app) => {
-  app.use("/", projectRoute);
-  app.use("/", taskRoute);
-};
-
-export default router;
